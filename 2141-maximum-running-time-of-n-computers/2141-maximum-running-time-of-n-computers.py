216c6147cbb3f81df6b0a56f2e4d47d7cf60f3ba
Neha-vundavilli2710 <23p31a4272@acet.ac.in>
2025-12-01 18:39:01 +0530

    Time: 1506 ms (19.05%), Space: 30.9 MB (91.43%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxRunTime(self, n: int, batteries: list[int]) -> int:
+        batteries.sort()
+        left, right = 0, sum(batteries) // n
+
+        def can_run(T):
+            total = 0
+            for b in batteries:
+                total += min(b, T)
+            return total >= n * T
+
+        while left < right:
+            mid = (left + right + 1) // 2
+            if can_run(mid):
+                left = mid
+            else:
+                right = mid - 1
+
+        return left
